1df5f406aca8dd2aab8c270a18b660ef2251b625
Diego Herrera Malambo <dherrerambo@gmail.com>
2022-08-03 11:47:05 -0500

    helpers

---
+++ helpers.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import os
+
+def load_dataset(name):
+    cwd = os.getcwd()
+    print(cwd)
+    os.chdir(os.path.dirname(cwd))
+    fName = f'_datasets\{name}.csv'
+    df = pd.read_csv(fName)
+    os.chdir(cwd)
+    print(df.shape)
+    print(df.info())
+    return df
